d8675f6f25e514552ca894c8db8ce7031e0c6eb7
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-07-22 18:15:21 +0300

    Update apiMoviedb.js

---
+++ src/services/apiMoviedb.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://api.themoviedb.org/3';
+const API_KEY = 'b8b4ca8ae31738ae90c3cb58e4b03190';
+
+const getTrending = async () => {
+  const resp = await axios.get(`/trending/movie/day${API_KEY}`);
+  return resp.data.results.map(({ id, title }) => {
+    return { id, title };
+  });
+};
+
+const searchMovie = async inputValue => {
+  const resp = await axios.get(
+    `/search/movie${API_KEY}&query=${inputValue}&language=en-US&page=1`);
+  return resp.data.results.map(({ id, title }) => {
+    return { id, title };
+  });
+};
+
+const getMovieDetails = async movieId => {
+  const resp = await axios.get(`/movie/${movieId}${API_KEY}&language=en-US`);
+  return resp.data;
+};
+
+const getMovieCredits = async moviesId => {
+  const resp = await axios.get(
+    `/movie/${moviesId}/credits${API_KEY}&language=en-US`);
+
+  return resp.data.cast.map(({ name, character, profile_path, id }) => {
+    return { name, character, profile_path, id };
+  });
+};
+
+export {getTrending, searchMovie, getMovieDetails, getMovieCredits};
