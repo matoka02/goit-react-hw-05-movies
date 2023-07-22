5f19fb161b42760ebb072aa32b7d4c3108ef58a9
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-07-22 20:19:58 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
+import SharedLayout from './SharedLayout/SharedLayout';
+import { Route, Routes } from 'react-router-dom';
+import { lazy } from 'react';
+
+const HomePage = lazy(()=>import('../pages/HomePage'));
+const MovieDetailsPage = lazy(()=>import('../pages/MovieDetailsPage'));
+const Movies = lazy(()=>import('../pages/Movies'));
+const Cast = lazy(()=>import('../components/Cast/Cast'));
+const Reviews = lazy(()=>import('../components/Reviews/Reviews'));
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route path='/' element={<SharedLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path='movies' element={<Movies />} />
+        <Route path='movies/:movieId' element={<MovieDetailsPage />} >
+          <Route path='cast' element={<Cast />}/>
+          <Route path='reviews' element={<Reviews />}/>
+        </Route>
+      </Route>
+    </Routes>
   );
 };
+
+
