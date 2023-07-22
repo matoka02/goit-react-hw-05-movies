7ef40b5a47ec545949de82aa268d858dc5c415ff
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-07-22 18:52:19 +0300

    Create SearchForm.jsx

---
+++ src/components/SearchForm/SearchForm.jsx
@@ -0,0 +1,35 @@
+import * as API from 'services/apiMoviedb';
+import { TrendingList } from 'components/TrendingList/TrendingList';
+import React, { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+
+
+const SearchForm = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const handleSubmit = evt => {
+    setSearchParams({ query: evt.target[0].value });
+    evt.preventDefault();
+    return;
+  };
+
+  useEffect(() => {
+    const movieTitle = searchParams.get('query');
+    if (movieTitle) {
+      API.searchMovies(movieTitle).then(ret => setMovies(ret));
+    }
+  }, [searchParams]);
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Search..."></input>
+        <button type="submit">Search</button>
+      </form>
+      <TrendingList movies={movies} />
+    </div>
+  );
+};
+
+export default SearchForm;
